eea3b13d18c1a7d4164efc6231dceab71d8ba1a9
CatsSavior <42435869+CatsSavior@users.noreply.github.com>
2019-07-29 12:36:11 +0300

    Update forms.py

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -2,13 +2,4 @@ from django import forms
 
 
 class UploadDocumentForm(forms.Form):
-    video = forms.FileField()
-    image = forms.ImageField()
-
-
-class Img(forms.Form):
-    image = forms.ImageField()
-
-
-class Video(forms.Form):
-    video = forms.FileField()
+    file = forms.FileField()
